683e70198eebc7b6e7aa578e25d3154bc95ebac4
Евгений <evgens@mail.ru>
2024-08-20 20:16:48 +0300

    CarLinkedList

---
+++ src/main/java/CarLinkedList.java
@@ -0,0 +1,106 @@
+public class CarLinkedList implements CarList {
+
+    private Node first;
+    private Node last;
+    private int size;
+
+    @Override
+    public Car get(int index) {
+        return getNode(index).value;
+    }
+
+    @Override
+    public void add(Car car) {
+        if (size == 0) {
+            first = new Node(null, car, null);
+            last = first;
+        } else {
+            Node secondLast = last;
+            last = new Node(secondLast, car, null);
+            secondLast.next = last;
+        }
+        size++;
+    }
+
+    @Override
+    public void add(Car car, int index) {
+        if (index < 0 || index > size) throw new IndexOutOfBoundsException();
+        if (index == size) {
+            add(car);
+            return;
+        }
+        Node nodeNext = getNode(index);
+        Node nodePrevious = nodeNext.previous;
+        Node newNode = new Node(nodePrevious, car, nodeNext);
+        nodeNext.previous = newNode;
+        if (nodePrevious != null) {
+            nodePrevious.next = newNode;
+        } else {
+            first = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public boolean remove(Car car) {
+        Node node = first;
+        for (int i = 0; i < size; i++) {
+            if (node.value.equals(car)) return removeAt(i);
+            node = node.next;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeAt(int index) {
+        Node node = getNode(index);
+        Node nodePriv = node.previous;
+        Node nodeNext = node.next;
+        if (nodePriv != null){
+            nodePriv.next = nodeNext;
+        }else{
+            first = nodeNext;
+        }
+        if (nodeNext != null){
+            nodeNext.previous = nodePriv;
+        }else{
+            last = nodePriv;
+        }
+        size--;
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void clear() {
+        first = null;
+        last = null;
+        size = 0;
+    }
+
+    private Node getNode(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        Node node = first;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    private static class Node {
+        private Node previous;
+        private Car value;
+        private Node next;
+
+        public Node(Node previous, Car value, Node next) {
+            this.previous = previous;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+}
---
+++ src/test/java/CarListTest.java
@@ -0,0 +1,87 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarListTest {
+//    private CarList carList;
+    private CarList carList;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+//        carList = new CarArrayList();
+        carList = new CarLinkedList();
+        for (int i = 0; i < 100; i++) {
+            carList.add(new Car("Brand" + i, i));
+        }
+    }
+
+    @Test
+    public void whenAdded100ElementsThenSizeMustDe100() {
+        assertEquals(100, carList.size());
+    }
+
+    @Test
+    public void whenElementRemovedByIndexThenSizMustBeDecreased() {
+        assertTrue(carList.removeAt(5));
+        assertEquals(99, carList.size());
+    }
+
+    @Test
+    public void whenElementRemovedThenSizMustBeDecreased() {
+        Car car = new Car("Toyota", 15);
+        carList.add(car);
+        assertEquals(101, carList.size());
+        assertTrue(carList.remove(car));
+        assertEquals(100, carList.size());
+    }
+
+    @Test
+    public void whenNonExistentElementRemovedThenReturnFalse() {
+        Car car = new Car("Toyota", 15);
+        assertFalse(carList.remove(car));
+        assertEquals(100, carList.size());
+    }
+
+    @Test
+    public void whenListClearedThenSizeMustBe0() {
+        carList.clear();
+        assertEquals(0, carList.size());
+    }
+
+
+    @Test //(expected = IndexOutOfBoundsException.class)
+    public void whenIndexOutOfBoundsThenThrownException() {
+        Exception exception = assertThrows(IndexOutOfBoundsException.class, () ->
+                carList.get(100));
+    }
+
+    @Test
+    public void methodGetReturnedRightValue() {
+        Car car = carList.get(0);
+        assertEquals("Brand0", car.getBrand());
+    }
+
+    @Test
+    public void insertIntoMiddle() {
+        Car car = new Car("BMW", 1);
+        carList.add(car, 50);
+        Car carFromList = carList.get(50);
+        assertEquals("BMW", carFromList.getBrand());
+    }
+
+    @Test
+    public void insertIntoFirst() {
+        Car car = new Car("BMW", 1);
+        carList.add(car, 0);
+        Car carFromList = carList.get(0);
+        assertEquals("BMW", carFromList.getBrand());
+    }
+
+    @Test
+    public void insertIntoLast() {
+        Car car = new Car("BMW", 1);
+        carList.add(car, 100);
+        Car carFromList = carList.get(100);
+        assertEquals("BMW", carFromList.getBrand());
+    }
+}
